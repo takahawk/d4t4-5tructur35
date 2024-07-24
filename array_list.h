7f11e4a5de677e20db0f0b48736d1cf98edad62b
w <ghostoftheshell@mailbox.org>
2024-07-24 15:53:11 +0300

    Add isEmpty function to array list

--- array_list.h
+++ array_list.h
@@ -1,5 +1,8 @@
 #ifndef ARRAY_LIST_H_
 #define ARRAY_LIST_H_
+
+#include <stdbool.h>
+
 typedef struct {
 	int elemSize;
 	int capacity;
@@ -28,6 +31,11 @@ AL_RemoveAt(ArrayList*, int);
 
 void
 AL_Swap(ArrayList*, int, int);
+
+inline bool
+AL_IsEmpty(ArrayList al) {
+	return al.len == 0;	
+}
 
 void
 FreeArrayList(ArrayList*);
